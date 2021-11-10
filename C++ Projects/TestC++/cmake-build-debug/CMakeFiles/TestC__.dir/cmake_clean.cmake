a4ca7a654dfb47dae0ef44cde8accd4e8832928a
John Elwart <johnelwart00@gmail.com>
2021-11-09 22:51:30 -0600

    Initial commit from CLion

---
+++ C++ Projects/TestC++/cmake-build-debug/CMakeFiles/TestC__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestC__.dir/main.cpp.obj"
+  "TestC__.exe"
+  "TestC__.exe.manifest"
+  "TestC__.pdb"
+  "libTestC__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
